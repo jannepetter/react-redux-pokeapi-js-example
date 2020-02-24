eb63c1e1b6f565fa206b00d8f36ac88065bab30b
Janne <peterdev80@gmail.com>
2020-02-24 12:50:06 +0200

    Updates

---
+++ precache-manifest.5ee7224c28a8afb91c00cbe052641adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7186e5a243d48d74179df47a0183efad",
+    "url": "/react-redux-pokeapi-js-example/index.html"
+  },
+  {
+    "revision": "ee26ee0981a09ffcaf31",
+    "url": "/react-redux-pokeapi-js-example/static/css/main.e61a5a5e.chunk.css"
+  },
+  {
+    "revision": "1627cfc871f72cb62139",
+    "url": "/react-redux-pokeapi-js-example/static/js/2.4e48afb6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-pokeapi-js-example/static/js/2.4e48afb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee26ee0981a09ffcaf31",
+    "url": "/react-redux-pokeapi-js-example/static/js/main.0647d311.chunk.js"
+  },
+  {
+    "revision": "41645941d5ad0689c41d",
+    "url": "/react-redux-pokeapi-js-example/static/js/runtime-main.5e0ed51a.js"
+  }
+]);
